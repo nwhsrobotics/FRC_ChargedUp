f6479265d45272107b809ca4337958489901880f
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-01-30 20:12:07 -0500

    Successfully setup PID for ShoulderSubsystem
    Setup-ed PID for shoulder subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -108,7 +108,13 @@ public final class Constants {
         public static final double kp = 1.0;
         public static final double ki = 0.0;
         public static final double kd = 0.0;
-        public static final int CanID20 = 20;
+        public static final double kIz = 0.0;
+        public static final double kFFz = 0.0;
+        public static final double kMaxOutput = 1.0;
+        public static final double kMinOutput = -1.0;
+        public static final int ShoulderCanID20 = 20;
+        public static final int ShoulderCanID21 = 21;
+
     }
     public static final class WristConstants {
         public static final double kp = 1.0;
