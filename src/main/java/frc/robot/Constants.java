b52d3d352dce04306c4be95495f936f7916b3410
DBARANETHARAN <55335467+DBARANETHARAN@users.noreply.github.com>
2023-01-31 19:45:17 -0500

    current autonomous

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
         public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
         public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
         public static final double kPTurning = .5; // P constant for turning
-        public static final double kPTolerance = 2.5 * (Math.PI/180);
+        //public static final double kPTolerance = 2.5 * (Math.PI/180);
         public static final double kITurning = 0.;
     }
 
@@ -75,7 +75,7 @@ public final class Constants {
     }
 
     public static final class AutoConstants {
-        public static final double kMaxSpeedMetersPerSecond = DriveConstants.kPhysicalMaxSpeedMetersPerSecond / 4;
+        public static final double kMaxSpeedMetersPerSecond = DriveConstants.kPhysicalMaxSpeedMetersPerSecond / 2;
         public static final double kMaxAngularSpeedRadiansPerSecond = DriveConstants.kPhysicalMaxAngularSpeedRadiansPerSecond;
 
         public static final TrajectoryConfig autoTrajectoryConfig =
@@ -84,9 +84,9 @@ public final class Constants {
                 Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
             .setKinematics(Constants.DriveConstants.kDriveKinematics);
         
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 5;
         public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI;
-        public static final double kPXController = 1;
+        public static final double kPXController = 1.5;
         public static final double kPYController = 1;
         public static final double kPThetaController = 0.25;
 
--- src/main/java/frc/robot/commands/SwerveAuto.java
+++ src/main/java/frc/robot/commands/SwerveAuto.java
@@ -25,7 +25,7 @@ public class SwerveAuto extends SequentialCommandGroup {
                 // Start at the origin facing the +X direction
                 new Pose2d(0, 0, new Rotation2d(0)),
                 // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1), new Translation2d(3, 0)),
+                List.of(new Translation2d(1, 1), new Translation2d(2, -1), new Translation2d(3, 0),  new Translation2d(3, 4), new Translation2d(3, 0), new Translation2d(0, -0.5), new Translation2d(-3, -0.5)),
                 // End 3 meters straight ahead of where we started, facing forward
                 new Pose2d(0, 0, new Rotation2d(0)),
                 Constants.AutoConstants.autoTrajectoryConfig);
