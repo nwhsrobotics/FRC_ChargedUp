93347265199b0c487622b96968c8a2d1fd2d9bdb
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-06 08:00:25 -0500

    Changed shoulderCommand to new class command of ShoulderCommand
    Changed shoulderCommand to new class command of ShoulderCommand

--- src/main/java/frc/robot/commands/ShoulderControl.java
+++ src/main/java/frc/robot/commands/ShoulderControl.java
@@ -4,42 +4,43 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShoulderSubsystem;
 
-public class ShoulderCommand extends CommandBase {
+public class ShoulderControl extends CommandBase {
   private final ShoulderSubsystem m_shoulderSubsystem;
-  private boolean isOn = true;
+  private double m_position;
   /** Creates a new shoulderCommand. */
-  public ShoulderCommand(ShoulderSubsystem subsystem) {
+  public ShoulderControl(ShoulderSubsystem subsystem, double position) {
     addRequirements(subsystem);
+    m_position = position;
+
     m_shoulderSubsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(isOn) {
-      m_shoulderSubsystem.resetPos();
+     m_shoulderSubsystem.setPos(m_position);
     }
-    else {
-      m_shoulderSubsystem.maxPos();
-    }
-  }
+
+  
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    isOn = false;
     return false;
 
   }
