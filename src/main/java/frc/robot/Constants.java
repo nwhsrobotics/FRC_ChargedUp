f653236acdda0e80397f776582ebb4d1111d90e4
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-06 08:15:10 -0500

    Updated most of the shoulder code and commented out the shoulder code in robotContainer to avoid dilemma
    Updated most of the shoulder code and commented out the shoulder code in robotContainer to avoid dilemma

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -105,18 +105,18 @@ public final class Constants {
     }
 
     public static final class ShoulderConstants {
-        public static final double kp = 1.0;
+        public static final double kp = 0.005;
         public static final double ki = 0.0;
         public static final double kd = 0.0;
         public static final double kIz = 0.0;
         public static final double kFFz = 0.0;
         public static final double kMaxOutput = 1.0;
         public static final double kMinOutput = -1.0;
-        public static final int ShoulderCanID20 = 27;
-        public static final int ShoulderCanID21 = 6;
-
-    }
-    public static final class WristConstants {
+        public static final int ShoulderCanID20 = 20; //changed id from 3 to 20 to avoid conflicts with other id values
+        public static final int ShoulderCanID21 = 21;
+    
+    } 
+        public static final class WristConstants {
         public static final double kp = 1.0;
         public static final double ki = 0.0;
         public static final double kd = 0.0;
