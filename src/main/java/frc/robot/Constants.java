5b4890a8608a61a56659a5636f848102395c4521
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-11 12:56:59 -0500

    Semi -working code
    U still need to push the motors maunally in the beginning tho

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -128,8 +128,8 @@ public final class Constants {
         public static final double kFFz = 0.0;
         public static final double kMaxOutput = 1.0;
         public static final double kMinOutput = -1.0;
-        public static final int ShoulderCanID20 = 3; // changed id from 3 to 20 to avoid conflicts with other id values
-        public static final int ShoulderCanID21 = 4;
+        public static final int ShoulderCanID20 = 4; // changed id from 3 to 20 to avoid conflicts with other id values
+        public static final int ShoulderCanID21 = 3;
     }
 
     public static final class WristConstants {
