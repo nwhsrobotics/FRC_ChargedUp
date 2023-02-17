5432c4412aaeb558f14424bff2045159a09cee59
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-17 09:21:23 -0500

    Put constants in constants.java file
    Put constants in constants.java file

--- src/main/java/frc/robot/subsystems/GrabberSubsystem.java
+++ src/main/java/frc/robot/subsystems/GrabberSubsystem.java
@@ -10,31 +10,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class GrabberSubsystem extends SubsystemBase {
 
   private DoubleSolenoid m_grabber;
+
   /** Creates a new GrabberSubsystem. */
-  public GrabberSubsystem() {
+  public GrabberSubsystem() 
+  {
     // Create a new DoubleSolenoid object for controlling a double solenoid on channels 0 and 1
-    //Setting grabber variable to the actual solenoid controlling the pneumatics
-    m_grabber = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
-    //Setting the current value of the solenoid to be off
+    // Setting grabber variable to the actual solenoid controlling the pneumatics
+    m_grabber = new DoubleSolenoid(PneumaticsModuleType.REVPH, GrabberConstants.forwardChannel, GrabberConstants.reverseChannel);
+    // Setting the current value of the solenoid to be off
     m_grabber.set(DoubleSolenoid.Value.kOff);
   }
 
-  public void grabberTurnOff() {
-  // Set the double solenoid to the off position
-  m_grabber.set(DoubleSolenoid.Value.kOff);
-  }
-  public void grabberExtend(){
-  // Extend the double solenoid grabber
-  m_grabber.set(DoubleSolenoid.Value.kForward);
+  public void grabberTurnOff() 
+  {
+    // Set the double solenoid to the off position
+    m_grabber.set(DoubleSolenoid.Value.kOff);
   }
 
-  public void grabberRetract(){
-  // Retract the double solenoid grabber
-  m_grabber.set(DoubleSolenoid.Value.kReverse);  
+  public void grabberExtend() 
+  {
+    // Extend the double solenoid grabber
+    m_grabber.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void grabberRetract() 
+  {
+    // Retract the double solenoid grabber
+    m_grabber.set(DoubleSolenoid.Value.kReverse);
   }
 
   @Override
-  public void periodic() {
+  public void periodic() 
+  {
     // This method will be called once per scheduler run
   }
 }
