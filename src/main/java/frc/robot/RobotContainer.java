a36c98eea0ed23d4f848c07b4d55f2678003d2fe
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-18 15:41:30 -0500

    Working degree change by 5 code
    Working degree change by 5 code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -54,7 +54,6 @@ public class RobotContainer {
         m_joyB.onTrue(m_shoulderPreset55deg);
         m_joyX.onTrue(m_shoulderPreset110deg);
 
-        m_joyLB.onTrue(m_extendedpresetlength1);
         m_joyRB.onTrue(m_extendedpresetlength3);
     }
 }
