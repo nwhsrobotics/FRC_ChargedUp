27f02d78e561677836434bda3a6200a3e40b2687
NewAmazingPVP <gamingofpat@gmail.com>
2023-02-21 22:24:26 -0500

    Set initialized commands for certain subsystems
    Set initialized commands for certain subsystems

--- src/main/java/frc/robot/commands/ShoulderControl.java
+++ src/main/java/frc/robot/commands/ShoulderControl.java
@@ -18,7 +18,9 @@ public class ShoulderControl extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
+  public void initialize() 
+  {
+    m_shoulderSubsystem.setPos(0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
