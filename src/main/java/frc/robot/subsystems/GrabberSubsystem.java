816ed59bcb014d9030dfc6daf132175dc1afdd2d
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-22 19:29:08 -0500

    REMOVED COMMENTS
    REMOVED COMMENTS to make the code cleaner

--- src/main/java/frc/robot/subsystems/GrabberSubsystem.java
+++ src/main/java/frc/robot/subsystems/GrabberSubsystem.java
@@ -12,34 +12,26 @@ public class GrabberSubsystem extends SubsystemBase {
   /** Creates a new GrabberSubsystem. */
   public GrabberSubsystem() 
   {
-    // Create a new DoubleSolenoid object for controlling a double solenoid on channels 0 and 1
-    // Setting grabber variable to the actual solenoid controlling the pneumatics
     m_grabber = new DoubleSolenoid(PneumaticsModuleType.REVPH, GrabberConstants.forwardChannel, GrabberConstants.reverseChannel);
-    // Setting the current value of the solenoid to be off
     m_grabber.set(DoubleSolenoid.Value.kOff);
   }
 
   public void grabberTurnOff() 
   {
-    // Set the double solenoid to the off position
     m_grabber.set(DoubleSolenoid.Value.kOff);
   }
 
   public void grabberExtend() 
   {
-    // Extend the double solenoid grabber
     m_grabber.set(DoubleSolenoid.Value.kForward);
   }
 
   public void grabberRetract() 
   {
-    // Retract the double solenoid grabber
     m_grabber.set(DoubleSolenoid.Value.kReverse);
   }
 
   @Override
-  public void periodic() 
-  {
-    // This method will be called once per scheduler run
+  public void periodic() {
   }
 }
