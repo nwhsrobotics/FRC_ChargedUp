80db5cccea346e529f8ca1d43efd6afba926f708
NewAmazingPVP <gamingofpat@gmail.com>
2023-02-22 19:29:08 -0500

    REMOVED COMMENTS
    REMOVED COMMENTS to make the code cleaner

--- src/main/java/frc/robot/subsystems/ExtendArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExtendArmSubsystem.java
@@ -8,63 +8,39 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ExtendArmConstants;
 
-// ExtendArmSubsystem initializes and sets up two brushless motors and their associated encoders and PID controllers
 public class ExtendArmSubsystem extends SubsystemBase {
 
-  // Declare two instances of the CANSparkMax motor controller class
   private CANSparkMax m_extendArmMotor1 = null;
-  // Declare two instances of the SparkMaxPIDController class
   private SparkMaxPIDController m_pidController1 = null;
-  // Declare two instances of the RelativeEncoder class
   private RelativeEncoder m_extendArmEncoder1 = null; 
-  // Set the current position for the shoulder motors
   private static double m_currentPos = 0.0;
-  // Set the desired position that user wants to go to for the shoulder motors
   private static double m_desiredPos = 0.0;
-  // Set the number of ticks per second
   private static final double TICKS_PER_SECOND = 50.0; // Revisit this value!!!
-  // Rotations it can make (Total_Distance) it can travel in one second
   private static final double TOTAL_DISTANCE = 25.0; // Revisit this value!!!
-  // Set the time it needs to reach the destination
   private static final double SECONDS_TO_MOVE = 1.0; // Revisit this value!!!
-  // Calculate the speed of rotation per tick (distance traveled per tick )
   private static final double SPEED_ROT_PER_TICK = ((TOTAL_DISTANCE)) / (SECONDS_TO_MOVE * TICKS_PER_SECOND);
-
   private double m_gearRatio = 18.9;
   private double m_oneRotationLength = 1.504; // in inches Revisit this values!!!
-
   private boolean m_enabled = false;
-  //private double m_resetDistance = 0.0;
 
   /** Creates a new ExtendArmSubsystem. */
   public ExtendArmSubsystem() {
-    // Initialize the first motor and set its PID controller and encoder
 
-    // creating an instance of CANSparkMax for the shoulder motor with ID ShoulderCanID20
     m_extendArmMotor1 = new CANSparkMax(ExtendArmConstants.ExtendArmCanID24, CANSparkMax.MotorType.kBrushless);
-    // checking if the shoulder motor instance is not null
+
     if (m_extendArmMotor1 != null) {
-      // getting PIDController instance from the shoulder motor
       m_pidController1 = m_extendArmMotor1.getPIDController();
-      // getting the encoder instance from the shoulder motor
       m_extendArmEncoder1 = m_extendArmMotor1.getEncoder();
-      // setting the encoder position to zero
       m_extendArmEncoder1.setPosition(0);
 
-      // setting the P, I, and D values for the PIDController from the ShoulderConstants
       m_pidController1.setP(ExtendArmConstants.kp);
       m_pidController1.setI(ExtendArmConstants.ki);
       m_pidController1.setD(ExtendArmConstants.kd);
-
-      // setting the IZone and FF values for the PIDController from the ShoulderConstants
       m_pidController1.setIZone(ExtendArmConstants.kIz);
       m_pidController1.setFF(ExtendArmConstants.kFFz);
 
-      // setting the output range for the PIDController from the ShoulderConstants
       m_pidController1.setOutputRange(ExtendArmConstants.kMinOutput, ExtendArmConstants.kMaxOutput);
-      // setting the reference for the PIDController to 0.0, using position control
       m_pidController1.setReference(0.0, ControlType.kPosition);
-      // printing a message to indicate the initialization of the extendArm motor 1
       System.out.println("ExtendArmMotor1 initialized");
       m_enabled = true;
     }
@@ -76,10 +52,8 @@ public class ExtendArmSubsystem extends SubsystemBase {
     System.out.println("desiredPos: " + m_desiredPos);
   }
 
-  // This method is called once per scheduler run. It is used to periodically update the motor position to match the desired position.  
   @Override
   public void periodic() {
-    //resetDistance = m_extendArmEncoder1.getPosition();
     if (m_enabled == true) {
       if((m_desiredPos > ((19 / m_oneRotationLength)* m_gearRatio))) 
       {
@@ -89,30 +63,22 @@ public class ExtendArmSubsystem extends SubsystemBase {
       {
         m_desiredPos = ((0 / m_oneRotationLength)* m_gearRatio);
       }
-      // Calculate the difference between the desired position and the current position
       double distance = (m_desiredPos - m_currentPos);
 
-      // Store the difference in a variable named delta
       double delta = distance;
 
-      // Check if delta is greater than the maximum speed(max distance it can travel in a tick) (SPEED_ROT_PER_TICK)
       if (delta > SPEED_ROT_PER_TICK) 
       {
-        // If delta is greater than the maximum speed(max distance it can travel in a tick), set delta to the maximum speed(max distance it can travel in a tick)
         delta = SPEED_ROT_PER_TICK;
       }
 
-      // Check if delta is less than the negative of the maximum speed(max distance it can travel in a tick)
       if (delta < -SPEED_ROT_PER_TICK) 
       {
-        // If delta is less than the negative of the maximum speed(max rotation it can travel in a tick), set delta to the negative of the maximum speed(max distance it can travel in a tick)
         delta = -SPEED_ROT_PER_TICK;
       }
 
-      // Update the current position by adding delta
       m_currentPos += delta;
 
-      // Set the reference position for the 2 PID controllers in two opposite directions
       m_pidController1.setReference(m_currentPos, ControlType.kPosition);
 
       SmartDashboard.putNumber("Extending Arm", m_extendArmEncoder1.getPosition());        
--- src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -8,31 +8,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShoulderConstants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// ShoulderSubsystem initializes and sets up two brushless motors and their associated encoders and PID controllers
 public class ShoulderSubsystem extends SubsystemBase 
 {
-  // Declare two instances of the CANSparkMax motor controller class
   private CANSparkMax m_shoulderMotor1 = null;
   public CANSparkMax m_shoulderMotor2 = null;
-  // Declare two instances of the SparkMaxPIDController class
   private SparkMaxPIDController m_pidController1 = null;
   private SparkMaxPIDController m_pidController2 = null;
-  // Declare two instances of the RelativeEncoder class
   private RelativeEncoder m_shoulderEncoder1 = null;
   private RelativeEncoder m_shoulderEncoder2 = null;
-
-  // Set the current position for the shoulder motors
   public static double m_currentPos = 0.0;
-  // Set the desired position that user wants to go to for the shoulder motors
   public static double m_desiredPos = 0.0;
-
-  // Set the number of ticks per second
   private static final double TICKS_PER_SECOND = 50.0; // Revisit this value!!!
-  // Rotations it can make (Total_Distance) it can travel
   private static final double TOTAL_DISTANCE = 61.1;
-  // Set the time it takes for the shoulder to reach destination
   private static final double SECONDS_TO_MOVE = 1.0; // Revisit this value!!!
-  // Calculate the speed of rotation per tick (distance traveled per tick )
   private static final double SPEED_ROT_PER_TICK = ((TOTAL_DISTANCE)) / (SECONDS_TO_MOVE * TICKS_PER_SECOND);
 
   private boolean m_enabled = false;
@@ -40,67 +28,41 @@ public class ShoulderSubsystem extends SubsystemBase
   /** Creates a new ShoulderSubsystem. */
   public ShoulderSubsystem() 
   {
-    // Initialize the first motor and set its PID controller and encoder
-
-    // creating an instance of CANSparkMax for the shoulder motor with ID ShoulderCanID20
     m_shoulderMotor1 = new CANSparkMax(ShoulderConstants.ShoulderCanID20, CANSparkMax.MotorType.kBrushless);
 
-    // checking if the shoulder motor instance is not null
     if (m_shoulderMotor1 != null) 
     {
-      // getting PIDController instance from the shoulder motor
       m_pidController1 = m_shoulderMotor1.getPIDController();
-      // getting the encoder instance from the shoulder motor
       m_shoulderEncoder1 = m_shoulderMotor1.getEncoder();
-      // setting the encoder position to zero
       m_shoulderEncoder1.setPosition(0);
 
-      // setting the P, I, and D values for the PIDController from the ShoulderConstants
       m_pidController1.setP(ShoulderConstants.kp);
       m_pidController1.setI(ShoulderConstants.ki);
       m_pidController1.setD(ShoulderConstants.kd);
-
-      // setting the IZone and FF values for the PIDController from the ShoulderConstants
       m_pidController1.setIZone(ShoulderConstants.kIz);
       m_pidController1.setFF(ShoulderConstants.kFFz);
 
-      // setting the output range for the PIDController from the ShoulderConstants
       m_pidController1.setOutputRange(ShoulderConstants.kMinOutput, ShoulderConstants.kMaxOutput);
-      // setting the reference for the PIDController to 0.0, using position control
       m_pidController1.setReference(0.0, ControlType.kPosition);
-      // printing a message to indicate the initialization of the shoulder motor 1
       System.out.println("ShoulderMotor1 initialized");
     }
 
-    // Initialize the second motor and set its PID controller and encoder
-
-    // creating an instance of CANSparkMax for the shoulder motor with ID ShoulderCanID21
     m_shoulderMotor2 = new CANSparkMax(ShoulderConstants.ShoulderCanID21, CANSparkMax.MotorType.kBrushless);
 
-    // checking if the shoulder motor instance is not null
     if (m_shoulderMotor2 != null) 
     {
-      // getting PIDController instance from the shoulder motor
       m_pidController2 = m_shoulderMotor2.getPIDController();
-      // getting the encoder instance from the shoulder motor
       m_shoulderEncoder2 = m_shoulderMotor2.getEncoder();
-      // setting the encoder position to zero
       m_shoulderEncoder2.setPosition(0);
 
-      // setting the P, I, and D values for the PIDController from the ShoulderConstants
       m_pidController2.setP(ShoulderConstants.kp);
       m_pidController2.setI(ShoulderConstants.ki);
       m_pidController2.setD(ShoulderConstants.kd);
-
-      // setting the IZone and FF values for the PIDController from the ShoulderConstants
       m_pidController2.setIZone(ShoulderConstants.kIz);
       m_pidController2.setFF(ShoulderConstants.kFFz);
 
-      // setting the output range for the PIDController from the ShoulderConstants
       m_pidController2.setOutputRange(ShoulderConstants.kMinOutput, ShoulderConstants.kMaxOutput);
-      // setting the reference for the PIDController to 0.0, using position control
       m_pidController2.setReference(0.0, ControlType.kPosition);
-      // printing a message to indicate the initialization of the shoulder motor 2
       System.out.println("ShoulderMotor2 initialized");
       m_enabled = true;
     }
@@ -116,7 +78,6 @@ public class ShoulderSubsystem extends SubsystemBase
   {
     if (m_enabled == true) 
     {
-      // This method is called once per scheduler run. It is used to periodically update the motor position to match the desired position.
       if(m_desiredPos > ((110 / 360) * 200))
       {
         m_desiredPos = ((110 / 360) * 200);
@@ -125,30 +86,22 @@ public class ShoulderSubsystem extends SubsystemBase
       {
         m_desiredPos = ((0 / 360) * 200);
       }
-      // Calculate the difference between the desired position and the current position
       double distance = (m_desiredPos - m_currentPos);
 
-      // Store the difference in a variable named delta
       double delta = distance;
 
-      // Check if delta is greater than the maximum speed(max distance it can travel in a tick) (SPEED_ROT_PER_TICK)
       if (delta > SPEED_ROT_PER_TICK) 
       {
-        // If delta is greater than the maximum speed(max distance it can travel in a tick), set delta to the maximum speed(max distance it can travel in a tick)
         delta = SPEED_ROT_PER_TICK;
       }
 
-      // Check if delta is less than the negative of the maximum speed(max distance it can travel in a tick)
       if (delta < -SPEED_ROT_PER_TICK) 
       {
-        // If delta is less than the negative of the maximum speed(max rotation it can travel in a tick), set delta to the negative of the maximum speed(max distance it can travel in a tick)
         delta = -SPEED_ROT_PER_TICK;
       }
 
-      // Update the current position by adding delta
       m_currentPos += delta;
 
-      // Set the reference position for the 2 PID controllers in two opposite directions
       m_pidController1.setReference(m_currentPos, ControlType.kPosition);
       m_pidController2.setReference(-m_currentPos, ControlType.kPosition);
 
