122dcb9a53ad9896636becdb4fd1fc03e2729516
DBARANETHARAN <55335467+DBARANETHARAN@users.noreply.github.com>
2023-02-24 16:17:01 -0500

    wrist

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -15,6 +15,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.WristConstants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //TODO: get arm position and adjust pitch
 
