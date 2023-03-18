a75ef66aac283daba8d7ca1371bf7c9599845316
nwhsrobotics <jagbotics4638@gmail.com>
2023-03-18 08:56:49 -0400

    auto paths

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.LoggerConstants;
 
 /**
